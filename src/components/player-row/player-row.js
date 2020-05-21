67dbbe490252d48cc81360e4d8e3f90ab88eac3f
Matthew Shortt <mattshortt4@gmail.com>
2020-05-21 01:28:55 -0400

    fix - non-unique key in player-row

--- src/components/player-row/player-row.js
+++ src/components/player-row/player-row.js
@@ -7,7 +7,7 @@ function PlayerRow({players, conference}) {
     return (
         <div className='uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-6@m uk-text-center' data-uk-grid>
             {Object.keys(_.pick(players, conference)).map(key =>
-                <div key={`player_${players[key].id}`}>
+                <div key={`player_${players[key].id}_${key}`}>
                     {<Player
                         id={players[key].id}
                         name={players[key].name}
