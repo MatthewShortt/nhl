1a2ad67e342d90676430c4eb523d6c90faf916f0
MatthewShortt <mattshortt4@gmail.com>
2021-05-31 15:55:02 -0400

    feat: use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React                              from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import PrivateRoute                       from '#components/route/private-route';
 import IsOnline                           from '#components/is-online/is-online';
 import LogonLayout                        from '#pages/logon/logon';
