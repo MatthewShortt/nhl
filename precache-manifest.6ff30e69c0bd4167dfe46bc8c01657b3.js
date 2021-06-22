2beac883ccc6109c89dd48a2db19c53deee34cab
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-22 02:33:15 +0000

    Deploy MatthewShortt/nhl to github.com/MatthewShortt/nhl.git:gh-pages

---
+++ precache-manifest.6ff30e69c0bd4167dfe46bc8c01657b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36cf22c8b44455b12c3765ed042de1f",
+    "url": "/nhl/index.html"
+  },
+  {
+    "revision": "7bea57732cc4e138ad73",
+    "url": "/nhl/static/css/main.15340bee.chunk.css"
+  },
+  {
+    "revision": "8f6bef76e4e827352358",
+    "url": "/nhl/static/js/2.9b3ed497.chunk.js"
+  },
+  {
+    "revision": "25c2cc9fea2093a591e72259458992ab",
+    "url": "/nhl/static/js/2.9b3ed497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bea57732cc4e138ad73",
+    "url": "/nhl/static/js/main.28fb952c.chunk.js"
+  },
+  {
+    "revision": "370ccdeb1d6c03f09a41",
+    "url": "/nhl/static/js/runtime-main.55015a7b.js"
+  }
+]);
